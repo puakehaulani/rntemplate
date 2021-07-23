07fc77317fd2827a72b7b5d69bb60c29cdfb882e
puakehaulani <puakehaulani@gmail.com>
2021-07-23 06:20:59 -0500

    home page started

---
+++ App/screens/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    Text,
+    StatusBar
+} from 'react-native';
+import colors from '../constants/colors';
+
+const screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
+
+export default () => {
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <Text>Home Page</Text>
+        </View>
+    )
+}
