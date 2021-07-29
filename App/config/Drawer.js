0d3995ea50eb258c01a26fcfa73a949b57960ad5
puakehaulani <puakehaulani@gmail.com>
2021-07-28 19:21:34 -0500

    added drawer nav to nesting

---
+++ App/config/Drawer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import MainStack from './MainStack';
+import Contact from '../screens/Contact';
+
+const Drawer = createDrawerNavigator();
+const DrawerNav = () => (
+    <Drawer.Navigator>
+        {/* <Drawer.Screen name="tab" component={TabNav} /> */}
+        <Drawer.Screen name="Home" component={MainStack} />
+        <Drawer.Screen name="Contact" component={Contact} />
+    </Drawer.Navigator>
+)
+
+export default () => {
+
+    return (
+        <DrawerNav />
+    )
+};
