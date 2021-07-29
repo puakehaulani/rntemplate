c0d72b190a2991640735fb302ed9231c8ac49d78
puakehaulani <puakehaulani@gmail.com>
2021-07-28 22:00:17 -0500

    moved common styling to external file and reattached

--- App/screens/Contact.js
+++ App/screens/Contact.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import {
     View,
-    StyleSheet,
-    Dimensions,
     Text,
     StatusBar,
-    Button
 } from 'react-native';
-import colors from '../constants/colors';
 
-const screen = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.white,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+import styles from '../constants/styles';
 
 export default () => {
     return (
--- App/screens/Settings.js
+++ App/screens/Settings.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import {
     View,
-    StyleSheet,
-    Dimensions,
     Text,
     StatusBar
 } from 'react-native';
-import colors from '../constants/colors';
 
-const screen = Dimensions.get('window');
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colors.white,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
+import styles from '../constants/styles';
 
 export default () => {
     return (
